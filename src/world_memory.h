46d24bd0782f4c85f78793c6847ae3d11c5ee2c2
parnet <parnet@stud.uni-frankfurt.de>
2021-11-03 22:17:55 +0100

    update check existence of file

--- src/world_memory.h
+++ src/world_memory.h
@@ -8,16 +8,12 @@
 #include "memory_observer.h"
 #include <vector>
 #include "lib_disc/function_spaces/grid_function.h"
-
 #include "lib_disc/time_disc/time_integrator_observers/time_integrator_observer_interface.h"
 
-
 namespace ug {
-
     namespace XBraidUtil {
 
-
-        unsigned long get_world_memory_consumed() {
+        unsigned long get_world_memory_consumed(){
             unsigned long world_memory = 0;
             auto this_proc_memory = get_physical_memory_consumed();
 
@@ -29,7 +25,7 @@ namespace ug {
             return world_memory;
         }
 
-        unsigned long get_spatial_memory_consumed() {
+        unsigned long get_spatial_memory_consumed(){
             unsigned long world_memory = 0;
             auto this_proc_memory = get_physical_memory_consumed();
 
@@ -51,8 +47,6 @@ namespace ug {
             unsigned long * sbuf = (unsigned long *)malloc(1*sizeof(unsigned long));
             unsigned long * rbuf = (unsigned long *)malloc(size*sizeof(unsigned long));
             sbuf[0] = get_physical_memory_consumed();
-
-
 
             MPI_Allgather(sbuf, 1, MPI_UNSIGNED_LONG,
                           rbuf, 1, MPI_UNSIGNED_LONG,
@@ -78,8 +72,6 @@ namespace ug {
             unsigned long * rbuf = (unsigned long *)malloc(size*sizeof(unsigned long));
             sbuf[0] = get_physical_memory_consumed();
 
-
-
             MPI_Allgather(sbuf, 1, MPI_UNSIGNED_LONG,
                           rbuf, 1, MPI_UNSIGNED_LONG,
                           PCL_COMM_WORLD);
@@ -89,7 +81,6 @@ namespace ug {
                 std::cout << i << ": " << rbuf[i] << std::endl;
             } // todo return for lua?
             std::cout << "Total : " << summ << std::endl;
-            //return distribution;
         }
 
 
@@ -111,12 +102,6 @@ namespace ug {
                 return true;
             }
         };
-
-
-
-
-
-
     }}
 
 #endif //UG_PLUGIN_XBRAIDUTIL_WORLD_MEMORY_H

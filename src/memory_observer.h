6d5d80c4517cca43392b36e4ae2aa1445e4cbc9a
parnet <parnet@stud.uni-frankfurt.de>
2021-12-01 07:43:03 +0100

    mgrit

--- src/memory_observer.h
+++ src/memory_observer.h
@@ -12,115 +12,29 @@
 #include <cstdio>
 #include <cstring>
 
-
 // basing on https://stackoverflow.com/questions/63166/how-to-determine-cpu-and-memory-consumption-from-inside-a-process
 namespace ug {
 
     namespace XBraidUtil {
 
+        unsigned long parseLine(char *line)
 
-        unsigned long parseLine(char *line){
-            unsigned long i = strlen(line);
-            const char *p = line;
-            while (*p < '0' || *p > '9') p++;
-            line[i - 3] = '\0';
-            i = atoi(p);
-            return i;
-        }
+        unsigned long get_virtual_memory_total()
 
-        unsigned long get_virtual_memory_total(){
-            struct sysinfo memInfo;
-            sysinfo (&memInfo);
-            unsigned long totalVirtualMem = memInfo.totalram;
-            totalVirtualMem += memInfo.totalswap;
-            totalVirtualMem *= memInfo.mem_unit;
-            return totalVirtualMem;
-        }
+        unsigned long get_virtual_memory_used()
 
-        unsigned long get_virtual_memory_used(){
-            struct sysinfo memInfo;
-            sysinfo (&memInfo);
-            unsigned long virtualMemUsed = memInfo.totalram - memInfo.freeram;
-            virtualMemUsed += memInfo.totalswap - memInfo.freeswap;
-            virtualMemUsed *= memInfo.mem_unit;
-            return virtualMemUsed;
-        }
+        unsigned long get_virtual_memory_consumed()
 
-        unsigned long get_virtual_memory_consumed(){
-            FILE *file = fopen("/proc/self/status", "r");
-            unsigned long result = -1;
-            char line[128];
+        unsigned long get_virtual_memory_peak()
 
-            while (fgets(line, 128, file) != nullptr) {
-                if (strncmp(line, "VmSize:", 7) == 0) {
-                    result = parseLine(line);
-                    break;
-                }
-            }
-            fclose(file);
-            return result;
-        }
+        unsigned long get_physical_memory_total()
 
-        unsigned long get_virtual_memory_peak(){
-            FILE *file = fopen("/proc/self/status", "r");
-            unsigned long result = -1;
-            char line[128];
+        unsigned long get_physical_memory_used()
 
-            while (fgets(line, 128, file) != nullptr) {
-                if (strncmp(line, "VmPeak:", 7) == 0) {
-                    result = parseLine(line);
-                    break;
-                }
-            }
-            fclose(file);
-            return result;
-        }
+        unsigned long get_physical_memory_consumed()
 
-        unsigned long get_physical_memory_total(){
-            struct sysinfo memInfo;
-            unsigned long totalPhysMem = memInfo.totalram;
-            totalPhysMem *= memInfo.mem_unit;
-            return totalPhysMem;
-        }
-
-        unsigned long get_physical_memory_used(){
-            struct sysinfo memInfo;
-            unsigned long physMemUsed = memInfo.totalram - memInfo.freeram;
-            physMemUsed *= memInfo.mem_unit;
-            return physMemUsed;
-        }
-
-        unsigned long get_physical_memory_consumed() {
-            FILE *file = fopen("/proc/self/status", "r");
-            unsigned long result = -1;
-            char line[128];
-
-            while (fgets(line, 128, file) != nullptr) {
-                if (strncmp(line, "VmRSS:", 6) == 0) {
-                    result = parseLine(line);
-                    break;
-                }
-            }
-            fclose(file);
-            return result;
-        }
-
-        unsigned long get_physical_memory_peak(){
-            FILE *file = fopen("/proc/self/status", "r");
-            unsigned long result = -1;
-            char line[128];
-
-            while (fgets(line, 128, file) != nullptr) {
-                if (strncmp(line, "VmHWM:", 6) == 0) {
-                    result = parseLine(line);
-                    break;
-                }
-            }
-            fclose(file);
-            return result;
-        }
+        unsigned long get_physical_memory_peak()
     }
 }
-
 
 #endif //UG_PLUGIN_XBRAIDUTIL_MEMORY_OBSERVER_H
